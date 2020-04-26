56abc278b8fea8e5cc5541adf4718f93923f26b1
robin <mgh3326@naver.com>
2020-04-26 23:05:15 +0900

    Zone domain and init data

---
+++ src/main/java/me/khmoon/studyolle/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package me.khmoon.studyolle.zone;
+
+import me.khmoon.studyolle.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
