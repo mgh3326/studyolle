0e7937a6bdf31b6f7869e2cf86be6212fdb4b5ac
robin <mgh3326@naver.com>
2020-08-15 23:40:04 +0900

    Add Study

---
+++ src/main/java/me/khmoon/studyolle/study/StudyService.java
@@ -0,0 +1,22 @@
+package me.khmoon.studyolle.study;
+
+
+import lombok.RequiredArgsConstructor;
+import me.khmoon.studyolle.domain.Account;
+import me.khmoon.studyolle.domain.Study;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class StudyService {
+
+  private final StudyRepository repository;
+
+  public Study createNewStudy(Study study, Account account) {
+    Study newStudy = repository.save(study);
+    newStudy.addManager(account);
+    return newStudy;
+  }
+}
